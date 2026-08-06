0b87dad1d7c4ab2549afc3a55f576f185b91ad74
Alexander Yurasov <ayurasov@gmail.com>
2026-08-07 01:09:25 +0300

    uiStore: добавить состояние sidebarCollapsed для сворачивающегося меню

--- src/stores/uiStore.js
+++ src/stores/uiStore.js
@@ -1,14 +1,22 @@
 import { defineStore } from 'pinia'
+import { LocalStorageAdapter } from '../repositories/storage/LocalStorageAdapter'
+
+const sidebarStorage = new LocalStorageAdapter('sidebar-collapsed')
 
 export const useUiStore = defineStore('ui', {
   state: () => ({
     openTaskId: null,
     quickCreateContext: null,
+    sidebarCollapsed: sidebarStorage.load(false),
   }),
   actions: {
     openTask(id) { this.openTaskId = id },
     closeTask() { this.openTaskId = null },
     openQuickCreate(context = {}) { this.quickCreateContext = context },
     closeQuickCreate() { this.quickCreateContext = null },
+    toggleSidebar() {
+      this.sidebarCollapsed = !this.sidebarCollapsed
+      sidebarStorage.save(this.sidebarCollapsed)
+    },
   },
 })
